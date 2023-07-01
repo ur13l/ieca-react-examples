50daecd72a866829927d0337e1a1843a1939ddb0
Uriel Infante <ur13l.infante@gmail.com>
2023-07-01 13:08:52 -0600

    [UPDATE] total price

--- 05-web-api/clase/fakestore-app/src/pages/Cart.jsx
+++ 05-web-api/clase/fakestore-app/src/pages/Cart.jsx
@@ -1,18 +1,22 @@
+import CartItem from "../components/CartItem";
 import { useCartContext } from "../provider/CartProvider";
 
 const Cart = () => {
   const {
-    state: { cart },
+    state: { cart, totalPrice },
   } = useCartContext();
 
   return (
     <div>
       <h1>Carrito</h1>
-      {cart.map((product) => (
-        <div key={product.id}>
-          <h2>{product.title}</h2>
+      <div className="flex justify-between items-center">
+        <div className="flex flex-col gap-4">
+          {cart.map((product, index) => (
+            <CartItem key={index} product={product} />
+          ))}
         </div>
-      ))}
+        <div className="font-bold text-3xl">TOTAL: ${totalPrice}</div>
+      </div>
     </div>
   );
 };
--- 05-web-api/clase/fakestore-app/src/provider/CartProvider.jsx
+++ 05-web-api/clase/fakestore-app/src/provider/CartProvider.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useReducer } from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
 
 const CartContext = createContext();
 
@@ -11,13 +11,37 @@ const reducer = (state, action) => {
         ...state,
         cart: [...state.cart, action.payload],
       };
+    case "REMOVE_FROM_CART":
+      return {
+        ...state,
+        cart: state.cart.filter((product) => product.id !== action.payload.id),
+      };
+    case "TOTAL_PRICE":
+      return {
+        ...state,
+        totalPrice: state.cart.reduce(
+          (prev, product) => prev + product.price,
+          0
+        ),
+      };
     default:
       return state;
   }
 };
 
 const CartProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, { cart: [] });
+  const [state, dispatch] = useReducer(
+    reducer,
+    JSON.parse(localStorage.getItem("state")) ?? { cart: [], totalPrice: 0 }
+  );
+
+  useEffect(() => {
+    localStorage.setItem("state", JSON.stringify(state));
+  }, [state]);
+
+  useEffect(() => {
+    dispatch({ type: "TOTAL_PRICE" });
+  }, [state.cart]);
 
   return (
     <CartContext.Provider value={{ state, dispatch }}>
